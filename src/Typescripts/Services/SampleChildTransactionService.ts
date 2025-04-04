2533c2649f23107f26ad90c8f367a11086d98007
dariodds <dario.dasilva.private@gmail.com>
2025-04-04 16:44:57 +0200

    extend functions

--- src/Typescripts/Services/SampleChildTransactionService.ts
+++ src/Typescripts/Services/SampleChildTransactionService.ts
@@ -4,6 +4,6 @@ import * as log from 'N/log';
 export class SampleChildTransactionService extends SampleParentTransactionService {
   override log (message: string) {
     super.log(message);
-    log.debug('SampleChildTransactionService', message);
+    log.debug('SampleChildTransactionService', `CHILD Generic : ${message}`);
   }
 }
