f0f9a9007425341e389796d6969f3f59045861dc
dariodds <dario.dasilva.private@gmail.com>
2025-04-07 14:47:27 +0200

    extend functions

--- src/Typescripts/Entities/SampleChildTransaction.ts
+++ src/Typescripts/Entities/SampleChildTransaction.ts
@@ -1,5 +1,8 @@
 import {SampleParentTransaction} from "@nsemea_lib/Entities/SampleParentTransaction";
+import * as record from 'N/record';
 
 export class SampleChildTransaction extends SampleParentTransaction {
-
+  override recordType() {
+    return record.Type.SALES_ORDER;
+  }
 }
