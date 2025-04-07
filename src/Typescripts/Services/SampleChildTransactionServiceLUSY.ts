95f717170584bfbceb9f69fe242259e6a8f1822b
dariodds <dario.dasilva.private@gmail.com>
2025-04-07 14:10:04 +0200

    extend functions

--- src/Typescripts/Services/SampleChildTransactionServiceLUSY.ts
+++ src/Typescripts/Services/SampleChildTransactionServiceLUSY.ts
@@ -1,9 +1,15 @@
 import * as log from 'N/log';
 import {SampleChildTransactionService} from "./SampleChildTransactionService";
+import {SampleParentTransactionServiceLUSY} from "@nsemea_lib/Services/SampleParentTransactionServiceLUSY";
 
 export class SampleChildTransactionServiceLUSY extends SampleChildTransactionService {
   override log (message: string) {
     super.log(message);
+    SampleParentTransactionServiceLUSY.logLusy1(message);
+    SampleParentTransactionServiceLUSY.logLusy2(message);
     log.debug('LUSY > SampleChildTransactionService', `CHILD LUSY Generic : ${message}`);
   }
 }
+
+// Apply the mixin to the class prototype
+Object.assign(SampleChildTransactionServiceLUSY.prototype, SampleParentTransactionServiceLUSY);
