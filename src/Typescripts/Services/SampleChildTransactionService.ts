9eb62d73562961803c020e5a8524e96336e963e0
dariodds <dario.dasilva.private@gmail.com>
2025-04-07 14:38:23 +0200

    extend functions

--- src/Typescripts/Services/SampleChildTransactionService.ts
+++ src/Typescripts/Services/SampleChildTransactionService.ts
@@ -1,9 +1,14 @@
 import {SampleParentTransactionService} from "@nsemea_lib/Services/SampleParentTransactionService";
 import * as log from 'N/log';
+import {SampleParentTransaction} from "@nsemea_lib/Entities/SampleParentTransaction";
 
 export class SampleChildTransactionService extends SampleParentTransactionService {
   override log (message: string) {
     super.log(message);
     log.debug('SampleChildTransactionService', `CHILD Generic : ${message}`);
+  }
+  override presetFields(SampleTransaction: SampleParentTransaction) {
+    super.presetFields(SampleTransaction);
+    log.debug('SampleChildTransactionService', 'Child Generic preset fields');
   }
 }
--- src/Typescripts/Services/SampleChildTransactionServiceLUSY.ts
+++ src/Typescripts/Services/SampleChildTransactionServiceLUSY.ts
@@ -1,6 +1,7 @@
 import * as log from 'N/log';
 import {SampleChildTransactionService} from "./SampleChildTransactionService";
 import {SampleParentTransactionServiceLUSY} from "@nsemea_lib/Services/SampleParentTransactionServiceLUSY";
+import {SampleParentTransaction} from "@nsemea_lib/Entities/SampleParentTransaction";
 
 export class SampleChildTransactionServiceLUSY extends SampleChildTransactionService {
   override log (message: string) {
@@ -8,6 +9,12 @@ export class SampleChildTransactionServiceLUSY extends SampleChildTransactionSer
     SampleParentTransactionServiceLUSY.logLusy1(message);
     SampleParentTransactionServiceLUSY.logLusy2(message);
     log.debug('LUSY > SampleChildTransactionService', `CHILD LUSY Generic : ${message}`);
+  }
+
+  override presetFields(SampleTransaction: SampleParentTransaction) {
+    super.presetFields(SampleTransaction);
+    SampleParentTransactionServiceLUSY.presetFieldsLUSY(SampleTransaction);
+    SampleTransaction.custbody_namecontactonsite += ' LUSY !!!'
   }
 }
 
