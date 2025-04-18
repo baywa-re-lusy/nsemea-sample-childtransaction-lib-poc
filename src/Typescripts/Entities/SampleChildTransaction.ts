a9b5253da0b98d26e01eb3254fa1406e9d345f10
dariodds <dario.dasilva.private@gmail.com>
2025-04-18 11:20:27 +0200

    add subrecord shipaddresslist

--- src/Typescripts/Entities/SampleChildTransaction.ts
+++ src/Typescripts/Entities/SampleChildTransaction.ts
@@ -1,7 +1,13 @@
 import {SampleParentTransaction} from "@nsemea_lib/Entities/SampleParentTransaction";
 import * as record from 'N/record';
+import {SubRecordDecorator} from "@nsemea_lib/Core/DataAccess/NSTypedRecord";
+import {AddressBase} from "@nsemea_lib/Core/DataAccess/AddressBase";
 
 export class SampleChildTransaction extends SampleParentTransaction {
+
+  @SubRecordDecorator(AddressBase)
+  accessor shipaddresslist: AddressBase;
+
   override recordType() {
     return record.Type.SALES_ORDER;
   }
